59ae5f79bcfff5724084f1f95058b94a05d405b5
Ayodejioladimeji <layobright11@gmail.com>
2024-01-04 16:34:40 +0100

    implemented the navbar and the login page

---
+++ src/components/nav.tsx
@@ -0,0 +1,184 @@
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+export default function Nav() {
+  const router = useRouter();
+
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    const userData = JSON.parse(
+      localStorage.getItem("kawa-admin-data") || "{}"
+    ).admin;
+    if (userData) {
+      setUser(`${userData.firstName} ${userData.lastName}`);
+    }
+  }, []);
+
+  return (
+    <nav className="navbar navbar-expand-lg align-items-center">
+      <div className="container-fluid">
+        <Link className="navbar-brand" href={"/"}>
+          WealthHat
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarScroll"
+          aria-controls="navbarScroll"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-between align-items-center"
+          id="navbarScroll"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link
+                className={`nav-link ${
+                  router.asPath.includes("DASHBOARD") ? "active" : ""
+                }`}
+                aria-current="page"
+                href="/overview"
+              >
+                Overview
+              </Link>
+            </li>
+
+            <li className="nav-item dropdown">
+              <Link
+                className={`nav-link dropdown-toggle ${
+                  router.asPath.includes("order-transactions") ||
+                  router.asPath.includes("payout-transactions")
+                    ? "active"
+                    : ""
+                }`}
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Finances
+              </Link>
+              <ul className="dropdown-menu">
+                <li>
+                  <Link href="/">Order Transactions</Link>
+                </li>
+                <li>
+                  <Link href="/">Payout Transaction</Link>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={`nav-link ${
+                  router.asPath.indexOf("/orders") === 0 ? "active" : ""
+                }`}
+                href="/"
+              >
+                Orders
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={`nav-link ${
+                  router.asPath.includes("/individual") ? "active" : ""
+                }`}
+                href="/"
+              >
+                Individuals
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                href="/"
+                className={`nav-link ${
+                  router.asPath.includes("/carriers") ? "active" : ""
+                }`}
+              >
+                Carriers
+              </Link>
+            </li>
+            <li className="nav-item dropdown">
+              <Link
+                className={`nav-link dropdown-toggle ${
+                  router.asPath.includes("settings") &&
+                  !router.asPath.includes("individual")
+                    ? "active"
+                    : ""
+                }`}
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Settings
+              </Link>
+              <ul className="dropdown-menu">
+                <li>
+                  <Link className="dropdown-item" href="/">
+                    General Settings
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" href="/">
+                    Reporting
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" href="/">
+                    Email Template
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" href="/">
+                    Subscriptions
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" href="/">
+                    States & Cities
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" href="/">
+                    Item Category
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <div className="d-flex align-items-center">
+            <li className="nav-item dropdown ms-4">
+              <Link
+                className={`nav-link dropdown-toggle d-flex gap-1 align-items-center`}
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <p>{user}</p>
+              </Link>
+              <ul className="dropdown-menu right">
+                <li
+                  onClick={() => {
+                    localStorage.removeItem("kawa-admin-data");
+                    router.push("/");
+                  }}
+                >
+                  <p>Log out</p>
+                </li>
+              </ul>
+            </li>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Script from "next/script";
+import "../styles/styles.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css"
+        integrity="sha512-YFENbnqHbCRmJt5d+9lHimyEMt8LKSNTMLSaHjvsclnZGICeY/0KYEeiHwD1Ux4Tcao0h60tdcMv+0GljvWyHg=="
+        crossOrigin="anonymous"
+        referrerPolicy="no-referrer"
+      />
+
+      <Script
+        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+        crossOrigin="anonymous"
+      ></Script>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+        integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
+        crossOrigin="anonymous"
+      ></Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
