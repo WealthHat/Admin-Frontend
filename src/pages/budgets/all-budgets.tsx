1540a8c111d5d250c3db3cca0d21bbc9d54a7e74
Ayodejioladimeji <layobright11@gmail.com>
2024-01-06 14:00:47 +0100

    created budgets pages

---
+++ src/pages/budgets/all-budgets.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "../../components/nav";
+
+interface Props {}
+
+const AllBudgets = (props: Props) => {
+  return (
+    <>
+      <Nav />
+      <div>All Budgets</div>
+    </>
+  );
+};
+
+export default AllBudgets;
---
+++ src/pages/budgets/create-budget.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "../../components/nav";
+
+interface Props {}
+
+const CreateBudget = (props: Props) => {
+  return (
+    <>
+      <Nav />
+      <div>Create Budgets</div>
+    </>
+  );
+};
+
+export default CreateBudget;
