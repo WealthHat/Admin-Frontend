adde20f36bbc237b347d9167bbca98c56c5ea014
Ayodejioladimeji <layobright11@gmail.com>
2024-01-06 14:12:00 +0100

    created networth pages

---
+++ src/pages/networths/all-networth.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "../../components/nav";
+
+interface Props {}
+
+const AllNetworth = (props: Props) => {
+  return (
+    <>
+      <Nav />
+      <div>All Budgets</div>
+    </>
+  );
+};
+
+export default AllNetworth;
